1fd53b804381c51b009996bd291bbb192162c91e
Sensei lee <aziwa1991@gmail.com>
2020-11-11 13:08:34 -0600

    Templates added

--- src/main/java/filter/AuthFilter.java
+++ src/main/java/filter/AuthFilter.java
@@ -6,6 +6,6 @@ import java.io.IOException;
 public class AuthFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        filterChain.doFilter(servletRequest, servletResponse);
+
     }
 }
